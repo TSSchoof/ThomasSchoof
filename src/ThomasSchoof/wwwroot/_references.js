ca8b2e2fb9973e80be84f5219d935eea3ed1dec1
TTSchoof <thomasschoof@gmail.com>
2016-08-06 22:01:04 +0200

    Add project files.

---
+++ src/ThomasSchoof/wwwroot/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../scripts/home.js" />
+/// <reference path="js/site.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/less/dist/less.js" />
